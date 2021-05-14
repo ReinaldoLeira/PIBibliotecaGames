315787f07ae06db7b6e4393b8306b790406ce461
juliout <juliocst1993@gmail.com>
2021-05-14 20:44:47 -0300

    ajustes da aula

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ const app = express();
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
-app.use(express.static(__dirname));
+
 
 app.use(logger('dev'));
 app.use(express.json());
