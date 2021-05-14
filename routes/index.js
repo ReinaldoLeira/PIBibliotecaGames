f91983ffa3513458de625846799024dcf1fb10a5
juliout <juliocst1993@gmail.com>
2021-05-14 06:42:45 -0300

    ajustes rotas

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,13 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
+const usersController = require('../controller/usersController');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
+
+router.get ('/login', usersController.login);
+router.get ('/registrar',usersController.registrar);
 
 module.exports = router;
