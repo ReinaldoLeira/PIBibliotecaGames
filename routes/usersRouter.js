c46316a0fc5469d6d1f3b0c40e5c1016af7f52c0
juliout <juliocst1993@gmail.com>
2021-07-15 12:17:32 -0300

    botao deletar post

--- routes/usersRouter.js
+++ routes/usersRouter.js
@@ -3,6 +3,7 @@ const router = express.Router();
 const usersController = require('../controller/usersController');
 const auth = require('../middlewares/auth');
 
+
 router.get ('/analise', auth, usersController.analise);
 
 router.get ('/form-analise', auth, usersController.formAnalise);
@@ -11,10 +12,13 @@ router.get ('/meusjogos', auth, usersController.meusJogos);
 
 router.get ('/posts', auth, usersController.posts);
 router.post ('/posts', auth, usersController.sendPosts);
+router.post('/posts/deletar/:id', auth , usersController.deletPost)
 
 router.get ('/', auth, usersController.usuario);
 router.post ('/', auth, usersController.sendConfigUsers);
 
 router.get ('/midias', auth, usersController.userMidias)
+
+
 
 module.exports = router
