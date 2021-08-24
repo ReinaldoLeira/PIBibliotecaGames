5608e3dd5c0dba0ab577e84ef10e59ee3d7e77b1
Bruno Grassi <brunoograssi@hotmail.com>
2021-08-24 19:12:35 -0300

    ajustes duvidas

--- public/js/index/duvidas.js
+++ public/js/index/duvidas.js
@@ -6,4 +6,11 @@ abrirDiv.onclick = () => {
     classe.classList.toggle('displayBlock')
 }
 
+const abrirDiv2 = document.querySelector('.faqTitulo2');
+const classe2 = document.querySelector('.faqDuvidas2');
+abrirDiv2.onclick = () => {
+    classe2.classList.toggle('displayBlock2')
+}
+
+
 });
