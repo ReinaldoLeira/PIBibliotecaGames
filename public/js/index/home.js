afa2e2aa227c6568f18111772d3f7395dec182d7
juliout <juliocst1993@gmail.com>
2021-08-24 11:19:44 -0300

    lista inicio amarela

--- public/js/index/home.js
+++ public/js/index/home.js
@@ -1,16 +1,26 @@
 let time = 2000, 
     currentDiv = 0,
+    currentList = 0,
     divs = document.querySelectorAll('.caixaJogos'),
-    max = divs.length;
+    lista = document.querySelectorAll('.listaJogo'),
+    max = divs.length,
+    max2 = lista.length;
+    divs[currentDiv].classList.add("aparecer")
+    lista[currentList].classList.add('amareloLista')
 
 function nextDiv() {
+      
     divs[currentDiv].classList.remove("aparecer")
+    lista[currentList].classList.remove('amareloLista')
 
     currentDiv++
-    if(currentDiv >= max){
+    currentList++
+    if(currentDiv >= max && currentList >= max2){
         currentDiv = 0
+        currentList = 0
     }
     divs[currentDiv].classList.add("aparecer")
+    lista[currentList].classList.add('amareloLista')
 }
 
 function start(){
